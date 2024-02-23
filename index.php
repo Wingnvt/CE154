2045174f67ecffecdd80a44a7bc01e39eb77e144
Davison <vd23025@essex.ac.uk>
2024-02-23 15:37:22 +0000

    hello

---
+++ index.php
@@ -0,0 +1,25 @@
+	<?php
+	include("inc/directory.php");
+	?>
+			<!-- right of the full container which contains the gallery of images -->
+			<div class="rightCon">
+										
+				<div id="imageBlock">
+					<a href="bubble.html"><img src="Images/smallBubble.png" alt="art of family in car" class="galleryImage"></a>
+					<a href="cat.html"><img src="Images/smallCat.jpg" alt="art of houses with cats" class="galleryImage"></a>
+					<a href="chapel.html"><img src="Images/smallChapel.png" alt="art of a chapel" class="galleryImage"></a>
+					<a href="fisherman.html"><img src="Images/smallFish.jpg" alt="art of people fishing" class="galleryImage"></a>
+					<a href="flood.html"><img src="Images/smallFlood.jpg" alt="art of a street being flooded" class="galleryImage"></a>
+					<a href="still.html"><img src="Images/smallStill.png" alt="art of an undead horseman" class="galleryImage"></a>
+					<a href="wait.html"><img src="Images/smallWait.png" alt="art of a plane crash with a survivor" class="galleryImage"></a>
+					<a href="wolf.html"><img src="Images/smallWolf.jpg" alt="art of a pack of wolfs surrounding a person" class="galleryImage"></a>
+				</div>
+			
+			</div>
+				<!-- will be fixed at bottom of page holding copyright info  -->
+			<div class="footer">
+				<p id="footerP">Property of Octavi Navarro ©2024 All Rights Reserved</p>
+			</div>
+		</div>
+	</body>
+</html>
